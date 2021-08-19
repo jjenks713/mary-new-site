1a074de1ecec507f828b10f6f8c564c67f099a06
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-08-19 12:43:26 -0600

    added unsplash api

--- public/js/index.js
+++ public/js/index.js
@@ -15,20 +15,36 @@ $(document).ready(function(){
       //var fileDom = $(data);
         console.log("testing get");
         console.log(data);
-        addKeyAjax(data);
+        var apiKey = data
+        addKeyAjax(apiKey);
     });
-    function addKeyAjax(data){
-        var url = "https://photoslibrary.googleapis.com/v1/albums";
-        url += '?' + $.param({
-            'api-key': data,
-        });
+    function addKeyAjax(apiKey){
+        var url = "https://api.unsplash.com/photos/?client_id=" + apiKey;
         console.log(url);
         $.ajax({
             url: url,
-            method: 'GET',
+            method: 'GET'
         }).done(function (result) {
-            console.log(result);
+           // var response = result.length;
+            for (var i = 0;i < result.length; i++){
+
+                console.log(result.length);
+                if (result[i].length){
+                    console.log(0);
+                    var photoDiv = $("<div></div>")
+                    var image = $("<img>")
+                    image.attr("src", result[i].urls.full);
+                    console.log(image);
+                    photoDiv.append(image);
+                    $("#photos-appear-here").prepend(photoDiv);
+                } else {
+                    console.log("Not working");
+                };
+
+            };
+        console.log(result);
         }).fail(function (err) {
+            //console.log(url);
             throw err;
         });
     };
