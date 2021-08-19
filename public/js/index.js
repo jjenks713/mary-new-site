d0f8a8e7b9b55c37d19d353619f30a1f32281508
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-08-19 09:10:16 -0600

    testing key

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+        trigger : 'hover'
+    });
+    $( "#modal-trigger" ).click(function() {
+      console.log("nothing");
+      //alert( "Handler for .click() called." );
+      $(".modal").show();
+    });
+    $("#MyModal").modal();
+
+
+    $.get('/key.txt', function(data) {
+      //var fileDom = $(data);
+        console.log("testing get");
+        console.log(data);
+    });
+
+        var url = "https://photoslibrary.googleapis.com/v1/albums";
+        url += '?' + $.param({
+            'api-key': "",
+        });
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+            console.log(result);
+        }).fail(function (err) {
+            throw err;
+        });
+});
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var app = express();
 /*   GET https://photoslibrary.googleapis.com/v1/albums
  */
 
-app.get("https://photoslibrary.googleapis.com/v1/albums");
-
 // Serve static content for the app from the "public" directory in the application directory.
 //app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
