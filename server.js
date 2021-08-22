22527b7e9a41b0e0daaacb232c22b0313c516664
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2021-08-22 13:03:03 -0600

    turned off email

--- server.js
+++ server.js
@@ -28,19 +28,19 @@ app.get("/drawings", function(req, res) {
     res.sendFile(path.join(__dirname, "drawings.html"));
     });
 // taken out so I dont receive emails on test
-app.post("/form", function(req, res){
+/* app.post("/form", function(req, res){
 
   var sendmail = require('sendmail')();
   sendmail({
       from: req.body.userEmail,
-      to: 'jenkin79@gmail.comasdf',
+      to: 'jenkin79@gmail.com',
       subject: req.body.userSubject,
       html: req.body.userText,
     }, function(err, reply) {
       console.log(err && err.stack);
       console.dir(reply);
   });
-});
+}); */
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
